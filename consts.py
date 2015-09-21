de1ce6f8ac90271c2667f3a62631a68f0ca72a0a
= <uran198@gmail.com>
2015-09-21 17:11:20 +0300

    add version number

--- consts.py
+++ consts.py
@@ -1,2 +1,2 @@
 scope = 'messages,friends,wall'
-oauth_link = "https://oauth.vk.com/authorize?client_id=4967910&redirect_uri=https://oauth.vk.com/blank.html&scope={scope}&display=mobile&response_type=token".format(scope=scope)
+oauth_link = "https://oauth.vk.com/authorize?client_id=4967910&redirect_uri=https://oauth.vk.com/blank.html&scope={scope}&display=mobile&response_type=token&v=5.37".format(scope=scope)
