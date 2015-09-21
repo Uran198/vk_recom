4bab3e0b4a17cb8cc1a447a7b97ecd591551066f
= <uran198@gmail.com>
2015-09-21 23:32:30 +0300

    add getting test data

--- gen_data.py
+++ gen_data.py
@@ -2,31 +2,41 @@ from vk import VK
 import time
 import json
 
-if __name__ == '__main__':
-	vk = VK()
-
+def get_newsfeed(vk, max_count):
 	params = {'filters'  : 'post' ,
 			  'count' : '100' }
-	params['end_time'] = int(time.time())
-	params['start_time'] = params['end_time'] - 60*60*24*50
+	params['start_time'] = 1
 	items = []
-	tot = 0
 	for _ in range(200):
 		feed = vk.call_method('newsfeed.get', params)
 		items += [ x for x in feed['items'] if len(x['text']) > 0]
 		params['start_from'] = feed.get('next_from', None)
-		print(feed.keys())
-		tot += len(feed['items'])
-		print(len(items), tot)
-		if not params['start_from']:
-			#del params['start_from']
-			#params['end_time'] = params['start_time']
-			#params['start_time'] = params['end_time'] - 60*60*24*20
+		print('Geting newsfeed', len(items), '/', max_count)
+		if not params['start_from'] or len(items) >= max_count:
 			break
-	#print(feed)
+	items = [ x for x in items if x['likes']['user_likes'] == 0 ]
+	return items
 
-	liked = [items[x] for x in range(len(items)) if items[x]['likes']['user_likes'] == 1]
-	print(len(liked))
+def get_favs(vk, max_count):
+	cnt, off, got = 100, 0, 1
+	items = []
+	params = { 'count' : str(cnt) }
+	while got > 0 and len(items) < max_count:
+		favs = vk.call_method('fave.getPosts', params)
+		items += [ x for x in favs['items'] if len(x['text']) > 0 ]
+		got = len(favs['items'])
+		off += cnt
+		params['offset'] = off
+		print('Getting favorites', len(items), '/', max_count)
+	return items
+
+if __name__ == '__main__':
+	vk = VK()
+	
+	recent = get_newsfeed(vk, 1000)
+	favs = get_favs(vk, 100)
+
+	data = recent + favs
 	fd = open('data.json', 'w')
-	json.dump(items, fd)
+	json.dump(data, fd)
 	fd.close()
