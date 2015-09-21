d23d88c95c64942b1a46b9bc286741b39e5edd9d
Roman Tymchyshyn <romantymchyshyn.rt@gmail.com>
2015-09-21 23:36:09 +0300

    small fix undefined reference to response

--- vk.py
+++ vk.py
@@ -1,9 +1,9 @@
-#/usr/bin/env python
+# /usr/bin/env python
 #
 #
 # config.json structure:
-#	{
-#		"uid": {
+# {
+# "uid": {
 #			"access_token":"your_token"
 #		}
 #	}
@@ -24,10 +24,12 @@ from enum import Enum
 
 #add error codes here for more obvious code
 class ErrorCode(Enum):
-    MuchRequestsError = 6
+	MuchRequestsError = 6
+
 
 logger = logging.getLogger('warning')
 logger.setLevel(logging.ERROR)
+
 
 class VK:
 	__api_url = "https://api.vk.com/method/"
@@ -37,7 +39,7 @@ class VK:
 	__timeout = 1
 	__retries = 3
 
-	def __init__(self, uid = None):
+	def __init__(self, uid=None):
 		if not self.__config_data:
 			self.__config_data = json.load(open(self.__config_file))
 		use_default = not uid
@@ -49,7 +51,7 @@ class VK:
 			uids = list(self.__config_data.keys())
 			logger.warning("Using defaul uid")
 			if len(uids) == 0:
-				logger.error("No available uids, please add some to '{0}'". format(self.__config_file))
+				logger.error("No available uids, please add some to '{0}'".format(self.__config_file))
 				exit(1)
 			else:
 				self.uid = uids[0]
@@ -57,19 +59,29 @@ class VK:
 			self.uid = uid
 		self.__access_token = self.__config_data[self.uid]['access_token']
 
+	def _get(self, url, params):
+		resp = None
+		retries = 0
+		while (not resp and retries < self.__retries):
+			try:
+				resp = requests.get(url, params=params, timeout=self.__timeout)
+			except requests.exceptions.Timeout:
+				sleep(self.__timeout)
+			retries += 1
+		return resp
+
 	def call_method(self, method, params):
-		req_url = self.__api_url+'/'+method
+		req_url = self.__api_url + '/' + method
 		params = params.copy()
 		params['access_token'] = self.__access_token
 		params['v'] = api_version
 		for _ in range(self.__retries):
-			for _ in range(self.__retries):
-				try:
-					resp = requests.get(req_url, params=params, timeout=self.__timeout)
-				except requests.exceptions.Timeout:
-					sleep(self.__timeout)
-					continue
-				break
+			resp = self._get(req_url, params)
+			if (not resp):
+				logger.error("Something gone wrong!")
+				logger.error('There are no response!')
+				exit(1)
+
 			resp.raise_for_status()
 			parsed = json.loads(resp.text)
 			if 'response' in parsed.keys():
@@ -87,6 +99,6 @@ if __name__ == '__main__':
 	vk = VK()
 
 	# get subscriptions
-	params = { 'user_id' : vk.uid }
+	params = {'user_id': vk.uid}
 	subscriptions = vk.call_method('users.getSubscriptions', params)
 	print(subscriptions)
